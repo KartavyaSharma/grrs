c325a5bae332abbb0b01f2f426945033e30a202f
Kartavya Sharma <kartavya@berkeley.edu>
2023-09-18 08:56:34 -0700

    Can read args and search in file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("pattern: {}, path: {}", args.pattern, args.path.display());
+    let content = std::fs::read_to_string(&args.path).expect("Could not read file!");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
